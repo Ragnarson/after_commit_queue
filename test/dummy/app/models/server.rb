ca3b912800cac4d4ea5b5d515075c57e5b441e98
Lucas Maxwell <lucas@thecowsays.mu>
2012-07-26 11:33:55 +1000

    Modify run_after_commit to accept blocks

--- test/dummy/app/models/server.rb
+++ test/dummy/app/models/server.rb
@@ -1,13 +1,16 @@
 class Server < ActiveRecord::Base
   include AfterCommitQueue
 
-  attr_accessor :started, :stopped
+  attr_accessor :started, :stopped, :meditating
 
   state_machine :state, :initial => :pending do
     after_transition :pending => :running, :do => :schedule_start
+    after_transition :running => :crashed, :do => :schedule_guru_meditation
     after_transition :running => :turned_off, :do => :schedule_stop
+
     event(:start) { transition :pending => :running }
     event(:stop) { transition :running => :turned_off }
+    event(:crash) { transition :running => :crashed }
   end
 
   def schedule_start
@@ -16,6 +19,12 @@ class Server < ActiveRecord::Base
 
   def schedule_stop
     run_after_commit(:stop_server)
+  end
+
+  def schedule_guru_meditation
+    run_after_commit do
+      @meditating = true
+    end
   end
 
   def start_server; @started = true end
